169cd718c8233b4f38e5c87633ad95189737400c
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2024-06-22 11:01:56 -0300

    feat: tunning connection pool

--- load-test.js
+++ load-test.js
@@ -1,11 +1,27 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 export const options = {
-vus: 2,
-duration: '300s',
+    vus: 2,
+    duration: '300s',
 };
 
+//export const options = {
+//  scenarios: {
+//    contacts: {
+//      executor: 'ramping-vus',
+//      preAllocatedVUs: 10,
+//      startVUs: 3,
+//      maxVus: 10
+//      stages: [
+//        { target: 20, duration: '30s' }, // linearly go from 3 VUs to 20 VUs for 30s
+//        { target: 100, duration: '0' }, // instantly jump to 100 VUs
+//        { target: 100, duration: '10m' }, // continue with 100 VUs for 10 minutes
+//      ],
+//    },
+//  },
+//};
+
 export default function () {
-let res = http.get('http://webflux-kotlin-log-context.example/characters/7');
+let res = http.get('http://webflux-kotlin-log-context.example/characters/suspend/7');
 check(res, { 'status was 200': (r) => r.status == 200 });
 }
--- src/main/kotlin/com/example/loggingcontextsample/configurations/WebClientConfig.kt
+++ src/main/kotlin/com/example/loggingcontextsample/configurations/WebClientConfig.kt
@@ -3,7 +3,12 @@ package com.example.loggingcontextsample.configurations
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import java.time.Duration
+
 
 @Configuration
 class WebClientConfig(
@@ -15,7 +20,18 @@ class WebClientConfig(
         @Value("\${client.base-url}")
         baseUrl: String,
     ): WebClient {
+        val provider = ConnectionProvider.builder("custom")
+            .maxConnections(1000)
+            .build()
+
+        val client = HttpClient
+            .create(provider)
+            .responseTimeout(
+                Duration.ofSeconds(5)
+            )
+
         return webClientBuilder
+            .clientConnector(ReactorClientHttpConnector(client))
             .baseUrl(baseUrl)
             .build()
     }
