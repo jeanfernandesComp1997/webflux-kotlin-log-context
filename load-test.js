7be85b54a96df1bead8d3ea3a78d664da2a32a18
jeanfernandesComp1997 <jeanfernandes10@hotmail.com>
2024-06-22 01:40:26 -0300

    fix: resources

--- load-test.js
+++ load-test.js
@@ -1,11 +1,11 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 export const options = {
-vus: 1,
+vus: 2,
 duration: '300s',
 };
 
 export default function () {
-let res = http.get('http://localhost:8080/characters/7');
+let res = http.get('http://webflux-kotlin-log-context.example/characters/7');
 check(res, { 'status was 200': (r) => r.status == 200 });
 }
